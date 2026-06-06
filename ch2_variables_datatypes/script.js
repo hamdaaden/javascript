65b523ac00f194a831fa508a10a9e897f1a1fc32
Hamda Aden <hamdaaden@Hamdas-MacBook-Air.local>
2026-06-06 15:46:45 +0100

    i added ch2_variable_datatypes

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,15 @@
+// var firstName = "Hamda" // string
+// var lastName = "Aden" // string
+// var age = 23 // number
+// var isStudent = true // boolean
+// console.log(firstName * lastName)
+// console.log(lastName)
+// console.log(age * age)
+// console.log(isStudent)
+
+// var town = "london"
+// console.log(town)
+var salary = 5000
+var tax = 500
+var takeHome = salary - 500
+console.log(takeHome)
